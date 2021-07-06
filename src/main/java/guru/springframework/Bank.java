3a16526145904c23cf53c8346f2b0a952a332f21
hal90 <norrisj0612@gmail.com>
2021-07-06 08:24:40 -0400

    Added Pair class and changed bank rate to actually check rates.

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -1,19 +1,28 @@
 package guru.springframework;
 
+import java.util.HashMap;
+
 /*
     Author: jalnor
     Date: 7/5/2021 10:25 AM
     Project: guru.springframework
 */
 public class Bank {
+
+    private HashMap<Pair, Integer> rateMap = new HashMap<>();
+
     Money reduce(Expression source, String toCurrency) {
         return source.reduce(this, toCurrency);
     }
 
     public int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        if (from.equals(to)) {
+            return 1;
+        }
+        return rateMap.get(new Pair(from, to));
     }
 
     public void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from, to), rate);
     }
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -34,8 +34,8 @@ public class Money implements Expression {
 
     @Override
     public Money reduce(Bank bank, String to) {
-        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
-        return new Money(amount / rate, to);
+//        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        return new Money(amount / bank.rate(this.currency, to), to);
         //return this;
     }
 
